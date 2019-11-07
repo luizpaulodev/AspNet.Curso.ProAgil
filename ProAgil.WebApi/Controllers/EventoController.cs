71f925f1e74903e7429771ff61741330cfa2bf6f
LuizPaulo <luizpaulodev@gmail.com>
2019-11-07 15:41:20 -0300

    Configurando Migrations e SQLite

--- ProAgil.WebApi/Controllers/EventoController.cs
+++ ProAgil.WebApi/Controllers/EventoController.cs
@@ -4,71 +4,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProAgil.WebApi.Data;
 using ProAgil.WebApi.Model;
 
 namespace ProAgil.WebApi.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("v1/[controller]")]
     public class EventoController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        public DataContext Context { get; }
 
-        private readonly ILogger<EventoController> _logger;
-
-        public EventoController(ILogger<EventoController> logger)
+        public EventoController(DataContext context)
         {
-            _logger = logger;
+            this.Context = context;
         }
 
         [HttpGet]
         public ActionResult<IEnumerable<Evento>> Get()
         {
-            return new Evento[] {
-                new Evento {
-                    EventoID = 1,
-                    Tema = "Angular",
-                    Local = "BH",
-                    Lote = "Primeiro Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToShortDateString()
-                },
-                new Evento {
-                    EventoID = 2,
-                    Tema = "Angular",
-                    Local = "SP",
-                    Lote = "Segundo Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToShortDateString()
-                }
-            };
+            return Context.Eventos.ToList();
         }
 
         [HttpGet("{id}")]
         public ActionResult<Evento> Get(int id)
         {
-            return new Evento[] {
-                new Evento {
-                    EventoID = 1,
-                    Tema = "Angular",
-                    Local = "BH",
-                    Lote = "Primeiro Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToShortDateString()
-                },
-                new Evento {
-                    EventoID = 2,
-                    Tema = "Angular",
-                    Local = "SP",
-                    Lote = "Segundo Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToShortDateString()
-                }
-            }.FirstOrDefault(x => x.EventoID == id);
+            return Context.Eventos.FirstOrDefault(x => x.EventoID == id);
         }
-        
     }
 }
